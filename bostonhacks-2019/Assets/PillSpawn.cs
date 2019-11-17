edaaecd7c234d45b298ac5ae15923fe3a20dc2c8
Ness <vanessa.chu@mail.mcgill.ca>
2019-11-16 23:15:58 -0500

    pill stats and spawn scripts, models

---
+++ bostonhacks-2019/Assets/PillSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PillSpawn : MonoBehaviour
+{
+    public string input;
+    public GameObject pill;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pill.AddComponent<PillStats>();
+        if (input.Equals("blue capsule"))
+        {
+            //TODO: THIS IS HARDCODED.
+            pill.GetComponent<PillStats>().mesh = AssetDatabase.LoadAssetAtPath<Mesh>("Assets/Models/blue_pill.fbx");
+            pill.GetComponent<PillStats>().location = "bedroom";
+            pill.GetComponent<PillStats>().audio = "Take one blue capsule.";
+        }
+        if (input.Equals("red pill"))
+        {
+            pill.GetComponent<PillStats>().mesh = AssetDatabase.LoadAssetAtPath<Mesh>("Assets/Models/red_pill.fbx");
+            pill.GetComponent<PillStats>().location = "bedroom";
+            pill.GetComponent<PillStats>().audio = "Take two red capsules.";
+        }
+
+        input = null;
+    }
+}
---
+++ bostonhacks-2019/Assets/PillStats.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PillStats : MonoBehaviour
+{
+    public Mesh mesh;
+    public string location;
+    public string audio;
+}
